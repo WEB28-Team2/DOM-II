29dfde3194bef904a283b46fdf9d6ccc01ac21d2
cjogles <jackson.ogles28@gmail.com>
2020-02-10 10:10:22 -0700

    added mouseover event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+// create a mouseover listener on fun bus image
+// that changes the width of the image and then returns 
+// the width back to original size.
+let funBusImg = document.getElementById('funbusimg');
+console.log(funBusImg);
+funBusImg.addEventListener('mouseover', function(event){
+  event.target.style.width = "10rem";
+  setTimeout(function(){
+    event.target.style.width = "100rem";
+  }, 500)
+}, );
